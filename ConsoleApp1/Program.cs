ba46e36015dc06ffed36b89b8c45aa1e6ce3d1c7
sagar0840 <sagarkale55556@gmail.com>
2023-03-30 21:34:00 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();// hello
+            string reverse = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reverse += str[i];
+            }
+            Console.WriteLine(reverse);
+            if (str == reverse)
+            {
+                Console.WriteLine("number is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("number is not palindrome");
+            }
+
+        }
+    }
+}
